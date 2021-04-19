7541c072d5a5fe989262d7043966240319f618d3
WashingtonBispo <Juniorbispo515@gmail.com>
2021-04-19 18:19:00 -0300

    Questão A - Terceiro commit
    Obter barra vermelha por novo teste para a mesma unidade previamente implentada.
    
    Co-authored-by: Sergio Cipriano <sergiosacj@protonmail.com>
    Co-authored-by: Eduarda Servidio <dudaservidio@gmail.com>

--- src/test/java/calculadoraCDB/TesteCalculoRendimentoBruto.java
+++ src/test/java/calculadoraCDB/TesteCalculoRendimentoBruto.java
@@ -12,4 +12,9 @@ public class TesteCalculoRendimentoBruto {
         Calculadora calculadora = new Calculadora(1000, 8.5, 60);
         assertEquals(13.97F, calculadora.getRendimentoBruto(), 0.01f);
     }
+
+    @Test public void TesteCalculoRendimentoBruto2() {
+        Calculadora calculadora = new Calculadora(500, 8.0, 120);
+        assertEquals(13.15F, calculadora.getRendimentoBruto(), 0.01f);
+    }
 }
