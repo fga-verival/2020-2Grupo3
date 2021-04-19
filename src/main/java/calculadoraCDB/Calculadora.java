b3dff273cd892ba2a0a58f14e2882db75d1bc962
WashingtonBispo <Juniorbispo515@gmail.com>
2021-04-19 19:00:18 -0300

    Questão A - Quarto commit
    Obter barra verde após oferecer implementação para a unidade sob desenvolvimento
    
    Co-authored-by: Sergio Cipriano <sergiosacj@protonmail.com>
    Co-authored-by: Eduarda Servidio <dudaservidio@gmail.com>

--- src/main/java/calculadoraCDB/Calculadora.java
+++ src/main/java/calculadoraCDB/Calculadora.java
@@ -2,21 +2,23 @@ package calculadoraCDB;
 
 public class Calculadora {
 
-    private double initalValue;
-    private double interest;
+    private double valorInicial;
+    private double juros;
     private double tempo;
+    private double aliquota;
 
-    public Calculadora(double initalValue, double interest, double tempo) {
-        this.initalValue = initalValue;
-        this.interest = interest;
-        this.tempo = initalValue;
+    public Calculadora(double valorInicial, double juros, double tempo) {
+        this.valorInicial = valorInicial;
+        this.juros = juros;
+        this.tempo = tempo;
+        aliquota = 0F;
+    }
+
+    public double getRendimentoBruto(){
+        return (valorInicial*tempo*juros)/(100*365);
     }
 
     public static void main(String[] args) {
 
-    }
-
-    public double getRendimentoBruto(){
-        return 13.97F; // Falsificação
     }
 }
--- src/test/java/calculadoraCDB/TesteCalculoRendimentoBruto.java
+++ src/test/java/calculadoraCDB/TesteCalculoRendimentoBruto.java
@@ -7,8 +7,7 @@ import calculadoraCDB.Calculadora;
 
 public class TesteCalculoRendimentoBruto {
 
-
-    @Test public void TesteCalculoRendimentoBruto1() {
+    @Test public void TesteCalculoRendimentoBruto1(){
         Calculadora calculadora = new Calculadora(1000, 8.5, 60);
         assertEquals(13.97F, calculadora.getRendimentoBruto(), 0.01f);
     }
