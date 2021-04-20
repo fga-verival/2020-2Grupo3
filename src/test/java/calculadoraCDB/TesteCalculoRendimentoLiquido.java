af167b5b8795a0d0dbef6347389431564dd8bdd0
WashingtonBispo <Juniorbispo515@gmail.com>
2021-04-19 23:10:15 -0300

    Questão C - Terceiro commit
    Obter barra vermelha por novo teste para a mesma unidade previamente
    implantada.
    
    Co-authored-by: Sergio Cipriano <sergiosacj@protonmail.com>
    Co-authored-by: Eduarda Servidio <dudaservidio@gmail.com>

--- src/test/java/calculadoraCDB/TesteCalculoRendimentoLiquido.java
+++ src/test/java/calculadoraCDB/TesteCalculoRendimentoLiquido.java
@@ -10,4 +10,9 @@ public class TesteCalculoRendimentoLiquido {
         Calculadora calculadora = new Calculadora(1000, 8.5, 60);
         assertEquals(1.0829, calculadora.getRendimentoLiquido(), 0.1F);
     }
+
+    @Test public void TesteCalculoRendimentoLiquido2() {
+        Calculadora calculadora = new Calculadora(500, 8, 120);
+        assertEquals(2.0384, calculadora.getRendimentoLiquido(), 0.1F);
+    }
 }
