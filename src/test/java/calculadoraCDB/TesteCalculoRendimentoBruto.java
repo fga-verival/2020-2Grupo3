d226dbb70569eb1facacd6d4ab555b07dc3b0a3b
WashingtonBispo <Juniorbispo515@gmail.com>
2021-04-19 21:12:47 -0300

    Questão A - Quinto commit
    Obter barra verde após novo teste escrito para a unidade. Triangularizado e parametrizado
    
    Co-authored-by: Sergio Cipriano <sergiosacj@protonmail.com>
    Co-authored-by: Eduarda Servidio <dudaservidio@gmail.com>

--- src/test/java/calculadoraCDB/TesteCalculoRendimentoBruto.java
+++ src/test/java/calculadoraCDB/TesteCalculoRendimentoBruto.java
@@ -1,19 +1,52 @@
 package calculadoraCDB;
 
-import org.junit.Test;
+import java.util.Arrays;
+import java.util.Collection;
 import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
+
 
 import calculadoraCDB.Calculadora;
 
+@RunWith(Parameterized.class)
 public class TesteCalculoRendimentoBruto {
 
-    @Test public void TesteCalculoRendimentoBruto1(){
-        Calculadora calculadora = new Calculadora(1000, 8.5, 60);
-        assertEquals(13.97F, calculadora.getRendimentoBruto(), 0.01f);
-    }
+	private double 	tempo,
+                    valorInicial,
+					juros,
+                    rendimentoBruto;
+	
 
-    @Test public void TesteCalculoRendimentoBruto2() {
-        Calculadora calculadora = new Calculadora(500, 8.0, 120);
-        assertEquals(13.15F, calculadora.getRendimentoBruto(), 0.01f);
-    }
+	public TesteCalculoRendimentoBruto (double tempo, double valorInicial, double juros, double rendimentoBruto) {
+		this.tempo = tempo;
+		this.valorInicial = valorInicial;
+		this.juros = juros;
+        this.rendimentoBruto = rendimentoBruto;
+	}
+	
+	@Parameters
+	public static Iterable<Object[]> getParameters() {
+		Object[][] resposta = new Object[][] {
+			{60F, 1000F, 8.5F, 13.97F},
+			{120F, 500F, 8F, 13.15F},
+			{240F, 3000F, 9F, 177.53F},
+			{420F, 250F, 8F, 23.01F},
+            {550F, 400F, 8F, 48.22F},
+            {670F, 800F, 8F, 117.48F},
+           {700F, 2500F, 9F, 431.51F}
+		};
+		
+		return Arrays.asList(resposta);
+	}
+	
+	@Test
+	public void test() {
+        Calculadora calculadora = new Calculadora(valorInicial, juros, tempo);
+        assertEquals(rendimentoBruto, calculadora.getRendimentoBruto(), 0.1F);
+	}
+
 }
