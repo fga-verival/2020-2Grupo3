3e580985356d033ae4e984708cc386e5f98c3e1b
WashingtonBispo <Juniorbispo515@gmail.com>
2021-04-19 22:20:46 -0300

    Questão B - Quinto commit
    Obter barra verde após novo teste escrito para a unidade.
    Triangularizado e parametrizado.
    
    Co-authored-by: Sergio Cipriano <sergiosacj@protonmail.com>
    Co-authored-by: Eduarda Servidio <dudaservidio@gmail.com>

--- src/test/java/calculadoraCDB/TesteImpostoRenda.java
+++ src/test/java/calculadoraCDB/TesteImpostoRenda.java
@@ -1,18 +1,50 @@
 package calculadoraCDB;
 
-import org.junit.Test;
+import java.util.Arrays;
+import java.util.Collection;
 import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
+
 
 import calculadoraCDB.Calculadora;
 
+@RunWith(Parameterized.class)
 public class TesteImpostoRenda {
-    @Test public void TesteCalculoImpostoRenda1() {
-        Calculadora calculadora = new Calculadora(1000, 8.5, 60);
-        assertEquals(3.14, calculadora.getImpostoRenda(), 0.1F);
-    }
+	private double 	tempo,
+                    valorInicial,
+					juros,
+                    ImpostoRenda;
+	
 
-    @Test public void TesteCalculoImpostoRenda2() {
-        Calculadora calculadora = new Calculadora(500, 8, 120);
-        assertEquals(2.96, calculadora.getImpostoRenda(), 0.1F);
-    }
+	public TesteImpostoRenda (double tempo, double valorInicial, double juros, double ImpostoRenda) {
+		this.tempo = tempo;
+		this.valorInicial = valorInicial;
+		this.juros = juros;
+        this.ImpostoRenda = ImpostoRenda;
+	}
+	
+	@Parameters
+	public static Iterable<Object[]> getParameters() {
+		Object[][] resposta = new Object[][] {
+			{60F, 1000F, 8.5F, 3.14F},
+			{120F, 500F, 8F, 2.96F},
+			{240F, 3000F, 9F, 35.51F},
+			{420F, 250F, 8F, 4.03F},
+            {550F, 400F, 8F, 8.44F},
+            {670F, 800F, 8F, 20.56F},
+           {700F, 2500F, 9F, 75.51F}
+		};
+		
+		return Arrays.asList(resposta);
+	}
+	
+	@Test
+	public void test() {
+        Calculadora calculadora = new Calculadora(valorInicial, juros, tempo);
+        assertEquals(ImpostoRenda, calculadora.getImpostoRenda(), 0.1F);
+	}
 }
